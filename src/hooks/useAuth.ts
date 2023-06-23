9e5f283f32ab85a5706416141d4c7e37127f6c3d
Matsumoto Ryota <awpjdmga1943@icloud.com>
2023-06-23 19:31:52 +0900

    feat: Add authentication

---
+++ src/hooks/useAuth.ts
@@ -0,0 +1,207 @@
+import {
+  getAuth,
+  signInWithPopup,
+  GoogleAuthProvider,
+  GithubAuthProvider,
+  getAdditionalUserInfo,
+  signInWithEmailAndPassword,
+  createUserWithEmailAndPassword,
+  sendEmailVerification,
+  ActionCodeSettings,
+} from "firebase/auth";
+import { getSession, signIn } from "next-auth/react";
+import { useRouter } from "next/router";
+import { useCallback, useEffect, useMemo, useState } from "react";
+import toast from "react-hot-toast";
+
+import { convertErrorMessageFromEnToJa } from "@/functions/firebase";
+
+import client from "@/lib/axios";
+import { firebaseApp } from "@/lib/firebase";
+
+import type { AuthProvider } from "firebase/auth";
+
+type UseAuth = (invitationCode: string | null) => {
+  pending: boolean;
+  handleGoogleSignIn: () => void;
+  handleGoogleSignUp: () => void;
+};
+
+// 定型メッセージ
+const PENDING = "処理中";
+const NEED_SIGN_UP =
+  "アカウントが存在しないか、無効なアカウントです。正しい情報を入力してください";
+const RECOMMEND_SIGN_IN =
+  "アカウントの作成ができませんでした。入力情報を確認して再度お試しください";
+const MUST_EMAIL_VERIFIED =
+  "メールアドレスが検証されていません。ご確認いただくか、確認メールの再送をしてください";
+
+const googleProvider = new GoogleAuthProvider();
+
+export const useAuth: UseAuth = (invitationCode) => {
+  const [pending, setPending] = useState(false);
+  const auth = getAuth(firebaseApp);
+  auth.languageCode = "ja";
+  const router = useRouter();
+  const redirectUrl = useMemo(() => {
+    if (typeof router.query.callbackUrl === "string") {
+      return `${process.env.NEXT_PUBLIC_APP_URL}${router.query.callbackUrl}`;
+    }
+    return `${process.env.NEXT_PUBLIC_APP_URL}`;
+  }, [router.query]);
+  useEffect(() => {
+    (async () => {
+      if (router.isReady) {
+        const session = await getSession();
+        // session情報がある場合、認証済みとしてリダイレクト
+        if (session !== null) {
+          router.replace(redirectUrl);
+        }
+      }
+    })();
+  }, [router.isReady]);
+  /**
+   * サインイン
+   */
+  // SNS認証
+  const handleOAuthSignIn = useCallback(
+    (provider: AuthProvider) => {
+      let toastId: string | null = toast.loading(PENDING);
+      setPending(true);
+      signInWithPopup(auth, provider)
+        // 認証に成功したら ID トークンを NextAuth に渡す
+        .then(async (credential) => {
+          const idToken = await credential.user.getIdToken(true);
+          const isNewUser = getAdditionalUserInfo(credential)?.isNewUser;
+          // 新規ユーザーであった場合エラーで終了
+          if (isNewUser) {
+            toast.error(NEED_SIGN_UP, {
+              id: toastId as string,
+            });
+            toastId = null;
+            credential.user.delete();
+            throw new Error(NEED_SIGN_UP);
+          }
+          return {
+            signInMethod: "sns",
+            refreshToken: credential.user.refreshToken,
+            idToken,
+          };
+        })
+        .then((result) => {
+          signIn("credentials", {
+            ...result,
+            callbackUrl: redirectUrl,
+          });
+        })
+        .catch((error) => {
+          let errorMessage: string | null = error.message;
+          const errorCode = error.code;
+          if (errorMessage !== NEED_SIGN_UP) {
+            errorMessage = convertErrorMessageFromEnToJa(errorCode, "signin");
+          }
+          if (toastId) {
+            toast.error(errorMessage, {
+              id: toastId as string,
+            });
+            toastId = null;
+          }
+        })
+        .finally(() => {
+          if (toastId) toast.dismiss(toastId);
+          setPending(false);
+        });
+    },
+    [auth, redirectUrl]
+  );
+  /**
+   * サインアップ
+   */
+  // SNS認証
+  const handleOAuthSignUp = useCallback(
+    (provider: AuthProvider) => {
+      let toastId: string | null = toast.loading(PENDING);
+      setPending(true);
+      signInWithPopup(auth, provider)
+        // 認証に成功したら新規登録API実行
+        .then(async (credential) => {
+          const isNewUser = getAdditionalUserInfo(credential)?.isNewUser;
+          // 登録済みユーザーであった場合エラーで終了
+          if (!isNewUser) {
+            toast.error(RECOMMEND_SIGN_IN, {
+              id: toastId as string,
+            });
+            toastId = null;
+            throw new Error(NEED_SIGN_UP);
+          }
+          return {
+            credential,
+          };
+        })
+        // 新規登録APIにリクエスト
+        .then(async ({ credential }) => {
+          const idToken = await credential.user.getIdToken(true);
+          try {
+            // await client.v1.companies.auth.$post({
+            //   body: {
+            //     invitation_code: invitationCode || "",
+            //   },
+            //   config: {
+            //     headers: {
+            //       Authorization: `Bearer ${idToken}`,
+            //     },
+            //   },
+            // });
+          } catch (error) {
+            credential.user.delete();
+            if (error instanceof Error) {
+              throw new Error(error.message);
+            }
+          }
+          return {
+            signInMethod: "sns",
+            refreshToken: credential.user.refreshToken,
+            idToken,
+          };
+        })
+        .then((result) => {
+          signIn("credentials", {
+            ...result,
+            callbackUrl: redirectUrl,
+          });
+        })
+        .catch((error) => {
+          let errorMessage: string | null = error.message;
+          const errorCode = error.code;
+          if (
+            errorMessage !== NEED_SIGN_UP &&
+            errorMessage !== RECOMMEND_SIGN_IN
+          ) {
+            errorMessage = convertErrorMessageFromEnToJa(errorCode, "signup");
+          }
+          if (toastId) {
+            toast.error(errorMessage, {
+              id: toastId as string,
+            });
+            toastId = null;
+          }
+        })
+        .finally(() => {
+          if (toastId) toast.dismiss(toastId);
+          setPending(false);
+        });
+    },
+    [auth, redirectUrl]
+  );
+  return {
+    pending,
+    handleGoogleSignIn: useCallback(
+      () => handleOAuthSignIn(googleProvider),
+      [handleOAuthSignIn]
+    ),
+    handleGoogleSignUp: useCallback(
+      () => handleOAuthSignUp(googleProvider),
+      [handleOAuthSignUp]
+    ),
+  };
+};
---
+++ src/lib/axios.ts
@@ -0,0 +1,43 @@
+import Axios, { AxiosRequestConfig } from "axios";
+import { getSession, signOut } from "next-auth/react";
+import { parse } from "qs";
+import qs from "qs";
+import toast from "react-hot-toast";
+
+const axios = Axios.create({
+  timeout: 30000,
+  baseURL: process.env.NEXT_PUBLIC_BASE_URL,
+  paramsSerializer: {
+    encode: parse as any,
+    serialize: (params) => qs.stringify(params, { arrayFormat: "brackets" }),
+  },
+  headers: {
+    "X-API-KEY": process.env.NEXT_PUBLIC_X_API_KEY,
+  },
+});
+
+const NO_TOAST_ENDPOINT_LIST = [""];
+
+axios.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  async (error) => {
+    let message =
+      error?.response?.data?.detail ||
+      error?.response?.data?.error ||
+      error.message;
+    const session = await getSession();
+    if (error?.response?.data?.status === 401) {
+      if (session) await signOut({ callbackUrl: "/sign_in" });
+      if (!error?.response?.data?.detail) message = "再認証が必要です";
+    }
+    const match = NO_TOAST_ENDPOINT_LIST.find((endpoint) =>
+      error.request.responseURL.includes(endpoint)
+    );
+    if (!match) toast.error(message);
+    return Promise.reject(error);
+  }
+);
+
+export default axios;
