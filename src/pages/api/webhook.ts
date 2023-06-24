bf133ff5cb0c900c4ae76d5adc3b8218f0a5551d
Matsumoto Ryota <awpjdmga1943@icloud.com>
2023-06-25 00:10:29 +0900

    feat: Create Webhook form

---
+++ src/pages/api/webhook.ts
@@ -0,0 +1,30 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from 'next'
+import {firebaseAdmin} from '@/lib/firebaseAdmin'
+
+const COLLECTION_NAME = 'users';
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+    const userId = req.body.id
+    const webhookUrl = req.body.webhookUrl
+    if (typeof userId !== 'string') {
+        return res.status(400).send('ユーザー情報の取得に失敗しました')
+    }
+    if (typeof webhookUrl !== 'string') {
+        return res.status(400).send('webhookのURLの取得に失敗しました')
+    }
+    const db = firebaseAdmin.firestore()
+    if (req.method === 'PUT') {
+        const docRef = db.collection(COLLECTION_NAME).doc(userId);
+    const result = await docRef.update({
+        webhook: webhookUrl
+    });
+    console.log(result)
+    res.status(200).json({ success: true })
+    } else {
+        res.status(404)
+    }
+}
