0a62d455089d424c6062c9314d0ed2ecd377cfd2
Matsumoto Ryota <awpjdmga1943@icloud.com>
2023-06-25 15:57:49 +0900

    feat: Add header of no authenticated

--- src/components/features/Header/index.tsx
+++ src/components/features/Header/index.tsx
@@ -2,12 +2,16 @@ import { signOut, useSession } from "next-auth/react";
 import React, { memo } from "react";
 import { UserProfile } from "./components/UserProfile";
 import { Spacer } from "../misc/Spacer";
+import { useRouter } from "next/router";
+import Link from "next/link";
 
 export const Header: React.FC = memo(() => {
   const { status } = useSession();
   return (
     <header className="w-screen flex px-4 py-2 border-b border-slate-600 gap-4 items-center">
-      <span>sample-slack-chat</span>
+      <Link href="/">
+        <span className="font-bold">sample-slack-chat</span>
+      </Link>
       {/* ローディング中 */}
       {status === "loading" && <></>}
       {/* 認証時 */}
@@ -25,7 +29,25 @@ export const Header: React.FC = memo(() => {
         </>
       )}
       {/* 未認証時 */}
-      {status === "unauthenticated" && <></>}
+      {status === "unauthenticated" && (
+        <>
+          <Spacer />
+          <Link
+            type="button"
+            className="inline-flex justify-center rounded-md border border-indigo-500 bg-transparent text-white px-4 py-2 text-sm font-medium hover:border-indigo-600 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2"
+            href="/sign_up"
+          >
+            sign up
+          </Link>
+          <Link
+            type="button"
+            className="inline-flex justify-center rounded-md border border-transparent bg-indigo-500 px-4 py-2 text-sm font-medium hover:bg-indigo-600 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2 text-white"
+            href="/sign_in"
+          >
+            sign in
+          </Link>
+        </>
+      )}
     </header>
   );
 });
