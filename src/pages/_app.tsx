3576ddd12ad813795764c82d4097db4abaf1fd92
Matsumoto Ryota <awpjdmga1943@icloud.com>
2023-06-26 01:10:24 +0900

    feat: Update react-query config

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,17 +4,7 @@ import { SessionProvider } from "next-auth/react";
 import { Toaster } from "react-hot-toast";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Header } from "@/components/features/Header";
-
-// Create a client
-const queryClient = new QueryClient({
-  defaultOptions: {
-    queries: {
-      refetchOnWindowFocus: false,
-      staleTime: Infinity,
-      retry: false,
-    },
-  },
-});
+import { queryClient } from "@/lib/react-query";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
