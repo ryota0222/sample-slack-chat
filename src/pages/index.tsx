ca361c595db7f5a254810e1acb1fe16d0d375984
Matsumoto Ryota <awpjdmga1943@icloud.com>
2023-06-25 15:57:00 +0900

    no authenticated page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,6 +4,7 @@ import { Loader } from "@/components/ui/Loader";
 import { WebhookForm } from "@/components/features/WebhookForm";
 import { MessageForm } from "@/components/features/MessageForm";
 import { MessageContent } from "@/components/features/MessageContent";
+import Link from "next/link";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -26,7 +27,31 @@ const Home = () => {
         </div>
       )}
       {/* 未認証時 */}
-      {status === "unauthenticated" && <>unauthenticated</>}
+      {status === "unauthenticated" && (
+        <div className="w-full py-12 justify-center grid gap-4">
+          <div className="text-center text-6xl">👨‍💻</div>
+          <p className="text-xl font-bold text-center">ログインはこちら</p>
+          <Link
+            type="button"
+            className="inline-flex justify-center rounded-md border border-transparent bg-indigo-500 px-4 py-2 text-sm font-medium hover:bg-indigo-600 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2 text-white"
+            href="/sign_in"
+          >
+            sign in
+          </Link>
+
+          <div className="text-center text-6xl mt-12">👩‍💻</div>
+          <p className="text-xl font-bold text-center">
+            アカウント登録はこちら
+          </p>
+          <Link
+            type="button"
+            className="inline-flex justify-center rounded-md border border-transparent bg-indigo-500 px-4 py-2 text-sm font-medium hover:bg-indigo-600 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2 text-white"
+            href="/sign_up"
+          >
+            sign up
+          </Link>
+        </div>
+      )}
     </main>
   );
 };
