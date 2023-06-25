eed40c5363a4aa690f760532328d3315f8557789
Matsumoto Ryota <awpjdmga1943@icloud.com>
2023-06-25 15:42:47 +0900

    feat: Update message ui

--- src/components/features/MessageForm/index.tsx
+++ src/components/features/MessageForm/index.tsx
@@ -9,7 +9,7 @@ export const MessageForm: React.FC = memo(() => {
   const mutation = usePostMessage({});
   const session = useSession();
   const queryClient = useQueryClient();
-  const { register, handleSubmit, setValue } = useForm();
+  const { register, handleSubmit, setValue, getValues } = useForm();
   const onSubmit = (data: any) => {
     if (data.message.length) {
       let toastId: string | null = toast.loading("処理中");
@@ -34,6 +34,11 @@ export const MessageForm: React.FC = memo(() => {
       );
     }
   };
+  const handleKeyDown = (e: React.KeyboardEvent<HTMLTextAreaElement>) => {
+    if (e.key === "Enter" && (e.ctrlKey || e.metaKey)) {
+      onSubmit(getValues());
+    }
+  };
   return (
     <div className="w-full py-12">
       <h2 className="mb-4 text-xl font-bold"># メッセージ送信</h2>
@@ -43,7 +48,8 @@ export const MessageForm: React.FC = memo(() => {
             id="message"
             {...register("message")}
             className="w-full p-2 rounded-md bg-gray-800 resize-y"
-            placeholder="メッセージを入力"
+            placeholder="⌘+Enter または Ctrl+Enter で送信"
+            onKeyDown={handleKeyDown}
           />
           <button
             type="submit"
